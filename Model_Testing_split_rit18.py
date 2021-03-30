6dd6145b529ddf0f68bd8959b1336bbf33d169cf
jaredwalker-git <74209997+jaredwalker-git@users.noreply.github.com>
2021-03-30 12:46:13 -0400

    Channels last setup
    Chipping and data changed to channels last for dense layers.

--- Model_Testing_split_rit18.py
+++ Model_Testing_split_rit18.py
@@ -39,21 +39,18 @@ def make_chips(image, chip_width, chip_height):
     # output shape (num chips, 6, 160, 160)
 
     dataset_of_chips = []
-
-    num_of_chips_x = train_data.shape[2] // chip_width
-    num_of_chips_y = train_data.shape[1] // chip_height
-
-    print("num chips left right:", train_data.shape[2] // chip_width)
-    print("num chips up down:", train_data.shape[1] // chip_height)
+    #Finding number of chips by dimensional resolution divided by desired size
+    num_of_chips_x = train_data.shape[1] // chip_width
+    num_of_chips_y = train_data.shape[0] // chip_height
 
     for i in range(num_of_chips_y):
         for j in range(num_of_chips_x):
-            dataset_of_chips.append(train_data[:,i*chip_height:(i+1)*chip_height, j*chip_width: (j+1)*chip_width])
+            dataset_of_chips.append(train_data[i*chip_height:(i+1)*chip_height, j*chip_width: (j+1)*chip_width,:])
 
     return np.array(dataset_of_chips)
 
 ###  
-filepath = "G:\OneDrive - University of Massachusetts Lowell - UMass Lowell\Github School\Dataset"
+filepath = "C:\\Users\\Jared\\Documents\\Datasets"
 os.chdir(filepath)
 
 file_path = 'rit18_data.mat'
@@ -61,36 +58,47 @@ file_path = 'rit18_data.mat'
 dataset = loadmat(file_path)
 #Load Training Data and Labels
 train_data = dataset['train_data']
-print(train_data.shape)
 
-train_mask = train_data[-1,:,:]
-train_data = train_data[:6,:,:]
+#moves bands to channel last
+train_data = np.moveaxis(train_data, 0, -1)
+print("Train Data shape: ", train_data.shape)
+
+#splitting 7th band of orthomosiac from train data and load train labels
+train_mask = train_data[:,:,-1]
+train_data = train_data[:,:,:6]
 train_labels = dataset['train_labels']
+print("Train Mask Shape: ", train_mask.shape)
 
-plt.imshow(train_data[5,:,:])
+
+plt.imshow(train_data[:,:,5])
 plt.show()
 chip_width, chip_height = (160,160)
+
 
 # show mask with grid
 # chips in the yellow region must be kept
 fig, ax = plt.subplots()
 plt.imshow(train_mask[:,:])
-grid_x = np.arange(0, train_data.shape[2], chip_width)
-grid_y = np.arange(0, train_data.shape[1], chip_width)
+grid_x = np.arange(0, train_data.shape[1], chip_width)
+grid_y = np.arange(0, train_data.shape[0], chip_width)
 ax.set_xticks(grid_x)
 ax.set_yticks(grid_y)
 ax.grid(which='both')
 plt.show()
 
+#prints number of chips 
+print("Number of chips in X:", train_data.shape[1] // chip_width)
+print("Number of chips in Y:", train_data.shape[0] // chip_height)
 train_data_chips =   make_chips(train_data, chip_width, chip_height)
 train_labels_chips = make_chips(train_labels, chip_width, chip_height)
 train_mask_chips =   make_chips(train_mask, chip_width, chip_height)
 
-print(train_data_chips.shape)
+print("Train Data Chip Shape: ", train_data_chips.shape)
 
-num_of_chips_x = train_data.shape[2] // chip_width
-num_of_chips_y = train_data.shape[1] // chip_height
+num_of_chips_x = train_data.shape[1] // chip_width
+num_of_chips_y = train_data.shape[0] // chip_height
 
+'''
 # get test locations from around the image for viewing
 # make sure test chips aren't from the edges of the images
 loc_1 = int(num_of_chips_x*0.4 + num_of_chips_x*num_of_chips_y*0.6)
@@ -101,31 +109,37 @@ show_these_chips = (loc_1, loc_2, loc_3)
 
 for index in show_these_chips:
 
-    plt.imshow(train_data_chips[index,5,:,:])
+    plt.imshow(train_data_chips[index,:,:,5])
     plt.show()
+'''
 
 #Load Validation Data and Labels
 val_data = dataset['val_data']
+val_data = np.moveaxis(val_data, 0, -1)
+print("Val Data Shape: ", val_data.shape)
 
-val_mask = val_data[-1]
-val_data = val_data[:6]
+val_mask = val_data[:,:,-1]
+val_data = val_data[:,:,:6]
 val_labels = dataset['val_labels']
 
 val_data_chips =   make_chips(val_data, chip_width, chip_height)
 val_labels_chips = make_chips(val_labels, chip_width, chip_height)
 val_mask_chips =   make_chips(val_mask, chip_width, chip_height)
 
-#Load Test Data
+#Load Test Data, Split mask from bands and create chips
 test_data = dataset['test_data']
-test_mask = test_data[-1]
-test_data = test_data[:6]
+test_data = np.moveaxis(test_data, 0, -1)
+print("Test Data Shape: ", test_data.shape)
 
-test_data_chips =   make_chips(test_data, chip_width, chip_height)
-test_mask_chips =   make_chips(test_mask, chip_width, chip_height)
+test_mask = test_data[:,:,-1]
+test_data = test_data[:,:,:6]
+
+test_data_chips = make_chips(test_data, chip_width, chip_height)
+test_mask_chips = make_chips(test_mask, chip_width, chip_height)
 
 
 #INPUT_Image = test_data_chips.reshape(2030,6,160,160)
-#print("INPUT_Image REsults:", INPUT_Image)
+#print("INPUT_Image Results:", INPUT_Image)
 
 
 #INPUT_Image = test_data_chips.reshape(6,160,160)
@@ -136,24 +150,25 @@ test_mask_chips =   make_chips(test_mask, chip_width, chip_height)
 model = Sequential()
 
 #encoder (down sampling)
-layer1 = (Conv2D(16, kernel_size=(3, 3), strides= 1,  padding ='same', activation='relu',  data_format='channels_first', input_shape=(6,160,160)))
+layer1 = (Conv2D(16, kernel_size=(3, 3), strides= 1,  padding ='same', activation='relu',  data_format='channels_last', input_shape=(160,160,6)))
 model.add(layer1)
-model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2), padding = 'valid', data_format = 'channels_first'))
-model.add(Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_first'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2), padding = 'valid', data_format = 'channels_first'))
-model.add(Conv2D(64, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_first'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2), padding = 'valid', data_format = 'channels_last'))
+model.add(Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2), padding = 'valid', data_format = 'channels_last'))
+model.add(Conv2D(64, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
 #decoder (up sampling)
-model.add(Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_first'))
-model.add(UpSampling2D(size=(2,2), data_format = 'channels_first'))
-model.add(Conv2D(16, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_first'))
-model.add(UpSampling2D(size=(2,2), data_format = 'channels_first'))
+model.add(Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
+model.add(UpSampling2D(size=(2,2), data_format = 'channels_last'))
+model.add(Conv2D(16, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
+model.add(UpSampling2D(size=(2,2), data_format = 'channels_last'))
 #model.add(Flatten())  #Add a “flatten” layer which prepares a vector for the fully connected layers
-model.add(Dense(160, activation='softmax'))
-model.add(Conv2D(6, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_first'))
+model.add(Dense(6, activation='softmax'))
+model.add(Conv2D(6, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
 #model.add(Activation('softmax'))
 
 #######################################################################
 
+'''
 #Create Custome Loss Function
 def band_reduction(layer1Weights, bandScore): 
     #index through filters, +1 score to bandScore for index of band that has highest weight 
@@ -178,6 +193,7 @@ for j in range(0, 6):
     plt.imshow(layer1Weights[:,:,j,3],interpolation="nearest",cmap="gray")   
 
 plt.show()
+'''
 
 #######################################################################
 
@@ -191,7 +207,7 @@ model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accur
 model.fit(train_data_chips, train_labels_chips, validation_data=(val_data_chips, val_labels_chips), batch_size=32, epochs=5, verbose=1, shuffle=True)
 
 #######################################################################
-
+'''
 #Plot 2D Conv Weights/Filters after training
 layer1Weights = layer1.get_weights()[0]
 
@@ -208,3 +224,4 @@ for j in range(0, 6):
     plt.imshow(layer1Weights[:,:,j,3],interpolation="nearest",cmap="gray")   
 
 plt.show()
+'''
