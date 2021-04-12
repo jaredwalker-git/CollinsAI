a30164b7f9ca850329f9389ad006372ed3ea1b8b
jaredwalker-git <74209997+jaredwalker-git@users.noreply.github.com>
2021-04-12 16:44:19 -0400

    Parallelism of band layers created
    Still needs tweaking on lambda layers for correct output shape

--- modelRIT18Training.py
+++ modelRIT18Training.py
@@ -16,7 +16,7 @@ import numpy as np
 #Import the Modules
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, UpSampling2D, Input, Concatenate
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, UpSampling2D, Input, Concatenate, Lambda, Layer
 from tensorflow.keras.models import Sequential
 
 from tensorflow.keras.losses import categorical_crossentropy 
@@ -140,9 +140,8 @@ print("Label chips for test \n", train_labels_softmax[1000,:])
 print("Total Number of Chips Taken from Mask: ", numChips)
 
 print("Train Data Chip Shape: ", train_data_chips.shape)
-print("Label Data Chip Shape: ", train_labels_softmax.shape) 
+print("Label Data Chip Shape: ", train_labels_softmax.shape)
 
-train_labels_softmax = [train_labels_softmax, train_labels_softmax]
 
 num_of_chips_x = train_data.shape[1] // chip_width
 num_of_chips_y = train_data.shape[0] // chip_height
@@ -155,32 +154,73 @@ plt.imshow(train_data_chips[1,:,:,5])
 
 
 #######################################################################
-#Create the parallel model structures
-classifier = Sequential()
-band_importance = Sequential()
+#input layer for all layers and lambda split 
+inputs = Input(shape = (40, 40, 6), batch_size = None)
+#print("input layer: ", inputs.shape)
 
-#parallel layers for importance through weight analysis
-importance_weights = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
-band_importance.add(Input(shape = (40, 40, 6)))
-band_importance.add(importance_weights)
+outputDenseLayers = []
+importance_weights = []
+
+#Creating split input and dense layer so each layer creates a weight for a single band -> must enumerate this for each band so that weights can be pulled by variable name
+inputDenseLayer1 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 6))(inputs)
+DenseLayer1 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
+Out1 = DenseLayer1(inputDenseLayer1)
+BandImportance1 = DenseLayer1.get_weights()
+importance_weights.append(BandImportance1)
+outputDenseLayers.append(Out1)
+print("input Layer 1: ", inputDenseLayer1.shape)
+#Band 2
+inputDenseLayer2 = Lambda(lambda x: x[:, :, 1], input_shape = (40, 40, 6))(inputs)
+DenseLayer2 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
+Out2 = DenseLayer2(inputDenseLayer2)
+BandImportance2 = DenseLayer2.get_weights()
+importance_weights.append(BandImportance2)
+outputDenseLayers.append(Out2)
+#Band 3
+inputDenseLayer3 = Lambda(lambda x: x[:, :, 2], input_shape = (40, 40, 6))(inputs)
+DenseLayer3 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
+Out3 = DenseLayer3(inputDenseLayer3)
+BandImportance3 = DenseLayer3.get_weights()
+importance_weights.append(BandImportance3)
+outputDenseLayers.append(Out3)
+#Band 4
+inputDenseLayer4 = Lambda(lambda x: x[:, 3], input_shape = (40, 40, 6))(inputs)
+DenseLayer4 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
+Out4 = DenseLayer4(inputDenseLayer4)
+BandImportance4 = DenseLayer4.get_weights()
+importance_weights.append(BandImportance4)
+outputDenseLayers.append(Out4)
+#Band 5
+inputDenseLayer5 = Lambda(lambda x: x[:, :, 4], input_shape = (40, 40, 6))(inputs)
+DenseLayer5 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
+Out5 = DenseLayer5(inputDenseLayer5)
+BandImportance5 = DenseLayer5.get_weights()
+importance_weights.append(BandImportance5)
+outputDenseLayers.append(Out5)
+#Band 6
+inputDenseLayer6 = Lambda(lambda x: x[:, :, 5], input_shape = (40, 40, 6))(inputs)
+DenseLayer6 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
+Out6 = DenseLayer6(inputDenseLayer6)
+BandImportance6 = DenseLayer6.get_weights()
+importance_weights.append(BandImportance6)
+outputDenseLayers.append(Out6)
+
+#now to concatenate the dense layers
+classifierInput = Concatenate(axis = 2)(outputDenseLayers)
+#print(classifierInput.shape)
 
 #encoder (down sampling)
-classifier.add(Input(shape = (40, 40, 6)))
-classifier.add(Conv2D(16, kernel_size=(3, 3), strides= 1,  padding ='same', activation='relu',  data_format='channels_last', input_shape=(40,40,6)))
-classifier.add(MaxPooling2D(pool_size=(2, 2), strides = 2, padding = 'valid', data_format = 'channels_last'))
-classifier.add(Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
-classifier.add(MaxPooling2D(pool_size=(2, 2), strides = 2, padding = 'valid', data_format = 'channels_last'))
-classifier.add(Conv2D(64, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
+C1 = Conv2D(16, kernel_size=(3, 3), strides= 1,  padding ='same', activation='relu',  data_format='channels_last', input_shape=(40,40,6))(classifierInput)
+P1 = MaxPooling2D(pool_size=(2, 2), strides = 2, padding = 'valid', data_format = 'channels_last')(C1)
+C2 = Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(P1)
+P2 = MaxPooling2D(pool_size=(2, 2), strides = 2, padding = 'valid', data_format = 'channels_last')(C2)
+C3 = Conv2D(64, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(P2)
 #decoder (up sampling)
-classifier.add(Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
-classifier.add(UpSampling2D(size=(2,2), data_format = 'channels_last'))
-classifier.add(Conv2D(16, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last'))
-classifier.add(UpSampling2D(size=(2,2), data_format = 'channels_last'))
-
-parallelLayers = ([classifier, band_importance])
-Concatenate()(parallelLayers)
-
-flattenLayer = Flatten()(parallelLayers)  #Add a “flatten” layer which prepares a vector for the fully connected layers
+C4 = Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(C3)
+U1 = UpSampling2D(size=(2,2), data_format = 'channels_last')(C4)
+C5 = Conv2D(16, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(U1)
+U2 = UpSampling2D(size=(2,2), data_format = 'channels_last')(C5)
+flattenLayer = Flatten()(output)  #Add a “flatten” layer which prepares a vector for the fully connected layers
 fullyConnected = Dense(16, activation='relu')(flattenLayer)
 output = Dense(3, activation='softmax')(fullyConnected)
 
@@ -192,11 +232,8 @@ trn_callback = tf.keras.callbacks.ModelCheckpoint(filepath=save_weights, save_we
 
 #######################################################################
 
-#Create summary of our parallel layers
-classifier.summary()
-band_importance.summary()
-
 #Compile the model 
+model = Model(input = data, output = output)
 model.compile(optimizer=Adam(lr = 0.00001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 BandWeights = importance_weights.get_weights()
