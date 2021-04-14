d20dda196ab8f06668e6da69917f7a81dcab841b
chandarapheng <71986977+chandarapheng@users.noreply.github.com>
2021-04-14 15:20:50 -0400

    Fixed inputDenselayer shape results format.

--- modelRIT18Training.py
+++ modelRIT18Training.py
@@ -25,8 +25,8 @@ import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
 
 
+#from keras import layers
 #from keras.layers import Lambda
-#from keras.backend import slice
 
 ###########################################################################
 def make_chips_data(image, chip_width, chip_height):
@@ -108,11 +108,11 @@ dataset_labels = loadmat('train_labels.mat')
 train_data = dataset['train_data']
 train_labels = dataset_labels['relabeled_training']
 
-#moves bands to channel last
+#Moves bands to channel last
 train_data = np.moveaxis(train_data, 0, -1)
 print("Train Data shape: ", train_data.shape)
 
-#splitting 7th band of orthomosiac from train data and load train labels
+#Splitting 7th band of orthomosiac from train data and load train labels
 train_mask = train_data[:,:,-1]
 train_data = train_data[:,:,:6]
 #print("Train Mask Shape: ", train_mask.shape)
@@ -123,8 +123,8 @@ plt.imshow(train_data[:,:,5])
 chip_width, chip_height = (40,40)
 
 
-# show mask with grid
-# chips in the yellow region must be kept
+#Show mask with grid
+#Chips in the yellow region must be kept
 fig, ax = plt.subplots()
 plt.imshow(train_mask[:,:])
 grid_x = np.arange(0, train_data.shape[1], chip_width)
@@ -134,7 +134,7 @@ ax.set_yticks(grid_y)
 ax.grid(which='both')
 #plt.show()
 
-#prints number of chips 
+#Prints number of chips 
 print("Number of chips in X:", train_data.shape[1] // chip_width)
 print("Number of chips in Y:", train_data.shape[0] // chip_height)
 train_data_chips =   make_chips_data(train_data, chip_width, chip_height)
@@ -151,14 +151,14 @@ num_of_chips_x = train_data.shape[1] // chip_width
 num_of_chips_y = train_data.shape[0] // chip_height
 
 
-#show chip 1 for proof of concept
+#Show chip 1 for proof of concept
 plt.imshow(train_data_chips[1,:,:,5])
 #plt.show()
 #END
 
 
 #######################################################################
-#input layer for all layers and lambda split 
+#Input layer for all layers and lambda split 
 inputsX =  Input(shape = (40, 40, 6), batch_size = None) #need to built model
 
 #print("input layer: ", inputs.shape)
@@ -168,7 +168,7 @@ importance_weights = []
 
 #Creating split input and dense layer so each layer creates a weight for a single band -> must enumerate this for each band so that weights can be pulled by variable name
 #Band 1
-inputDenseLayer1 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 1))(inputsX)
+inputDenseLayer1 = Lambda(lambda x: x[:, :, :, 0:1], input_shape = (40, 40, 6))(inputsX)
 DenseLayer1 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
 Out1 = DenseLayer1(inputDenseLayer1)
 BandImportance1 = DenseLayer1.get_weights()
@@ -176,35 +176,37 @@ importance_weights.append(BandImportance1)
 outputDenseLayers.append(Out1)
 print("input Layer 1: ", inputDenseLayer1.shape)
 #Band 2
-inputDenseLayer2 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 2))(inputsX)
+inputDenseLayer2 = Lambda(lambda x: x[:, :, :, 1:2], input_shape = (40, 40, 6))(inputsX)
 DenseLayer2 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
 Out2 = DenseLayer2(inputDenseLayer2)
 BandImportance2 = DenseLayer2.get_weights()
 importance_weights.append(BandImportance2)
 outputDenseLayers.append(Out2)
+print("input Layer 2: ", inputDenseLayer2.shape)
 #Band 3
-inputDenseLayer3 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 3))(inputsX)
+inputDenseLayer3 = Lambda(lambda x: x[:, :, :, 2:3], input_shape = (40, 40, 6))(inputsX)
 DenseLayer3 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
 Out3 = DenseLayer3(inputDenseLayer3)
 BandImportance3 = DenseLayer3.get_weights()
 importance_weights.append(BandImportance3)
 outputDenseLayers.append(Out3)
+print("input Layer 3: ", inputDenseLayer3.shape)
 #Band 4
-inputDenseLayer4 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 4))(inputsX)
+inputDenseLayer4 = Lambda(lambda x: x[:, :, :, 3:4], input_shape = (40, 40, 6))(inputsX)
 DenseLayer4 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
 Out4 = DenseLayer4(inputDenseLayer4)
 BandImportance4 = DenseLayer4.get_weights()
 importance_weights.append(BandImportance4)
 outputDenseLayers.append(Out4)
 #Band 5
-inputDenseLayer5 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 5))(inputsX)
+inputDenseLayer5 = Lambda(lambda x: x[:, :, :, 4:5], input_shape = (40, 40, 6))(inputsX)
 DenseLayer5 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
 Out5 = DenseLayer5(inputDenseLayer5)
 BandImportance5 = DenseLayer5.get_weights()
 importance_weights.append(BandImportance5)
 outputDenseLayers.append(Out5)
 #Band 6
-inputDenseLayer6 = Lambda(lambda x: x[:, :, :], input_shape = (40, 40, 6))(inputsX)
+inputDenseLayer6 = Lambda(lambda x: x[:, :, :, 5:6], input_shape = (40, 40, 6))(inputsX)
 DenseLayer6 = Dense(1, activation = None, kernel_regularizer = tf.keras.regularizers.l1(0.001))
 Out6 = DenseLayer6(inputDenseLayer6)
 BandImportance6 = DenseLayer6.get_weights()
@@ -212,25 +214,24 @@ importance_weights.append(BandImportance6)
 outputDenseLayers.append(Out6)
 
 ##########Testing
-print("input Layer 2: ", inputDenseLayer2.shape)
-print("input Layer 3: ", inputDenseLayer3.shape)
 print("input Layer 4: ", inputDenseLayer4.shape)
 print("input Layer 5: ", inputDenseLayer5.shape)
 print("input Layer 6: ", inputDenseLayer6.shape)
 
 
-#now to concatenate the dense layers
+#Now to concatenate the dense layers
 classifierInput = Concatenate(axis = 2)(outputDenseLayers)
 #print(classifierInput.shape)
+#############################################################################################################
 
 
-#encoder (down sampling)
+#Encoder (down sampling)
 C1 = Conv2D(16, kernel_size=(3, 3), strides= 1,  padding ='same', activation='relu',  data_format='channels_last', input_shape=(40,40,6))(classifierInput)
 P1 = MaxPooling2D(pool_size=(2, 2), strides = 2, padding = 'valid', data_format = 'channels_last')(C1)
 C2 = Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(P1)
 P2 = MaxPooling2D(pool_size=(2, 2), strides = 2, padding = 'valid', data_format = 'channels_last')(C2)
 C3 = Conv2D(64, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(P2)
-#decoder (up sampling)
+#Decoder (up sampling)
 C4 = Conv2D(32, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(C3)
 U1 = UpSampling2D(size=(2,2), data_format = 'channels_last')(C4)
 C5 = Conv2D(16, kernel_size=(3, 3), strides= 1, padding ='same', activation='relu', data_format='channels_last')(U1)
@@ -241,7 +242,7 @@ output = Dense(3, activation='softmax')(fullyConnected)
 
 #######################################################################
 
-# Create a callback that saves the model's weights
+#Create a callback that saves the model's weights
 save_weights = "training_weights_FINAL.ckpt"
 trn_callback = tf.keras.callbacks.ModelCheckpoint(filepath=save_weights, save_weights_only=True)
 
